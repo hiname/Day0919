f0b199ea0dd653917735b1cb2fc153d078d3201e
USER <aa@aa.com>
2016-09-21 14:54:56 +0900

    modify2

---
+++ app/src/main/java/com/day0919/ActTablelayout.java
@@ -0,0 +1,13 @@
+package com.day0919;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActTablelayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tablelay);
+    }
+}
--- app/src/main/java/com/day0919/ActViewClassTest.java
+++ app/src/main/java/com/day0919/ActViewClassTest.java
@@ -1,13 +1,11 @@
 package com.day0919;
 
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.widget.TextView;
+import android.widget.Switch;
 import android.widget.Toast;
 
 public class ActViewClassTest extends AppCompatActivity {
@@ -29,39 +27,51 @@ public class ActViewClassTest extends AppCompatActivity {
             public void onClick(View v) {
                 int num1 = Integer.parseInt(String.valueOf(input.getText()));
                 int num2 = Integer.parseInt(String.valueOf(input2.getText()));
-                Toast.makeText(getApplicationContext(), num1 + num2, Toast.LENGTH_LONG).show();
+                String result = (num1 + num2) + "";
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             }
         });
 
         btnMinus.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                int num1 = Integer.parseInt(input.getText().toString());
-                int num2 = Integer.parseInt(input2.getText().toString());
-                Toast.makeText(getApplicationContext(), num1 - num2, Toast.LENGTH_LONG).show();
+                int num1 = Integer.parseInt(String.valueOf(input.getText()));
+                int num2 = Integer.parseInt(String.valueOf(input2.getText()));
+                String result = (num1 - num2) + "";
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             }
         });
 
         btnMult.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                int num1 = Integer.parseInt(input.getText().toString());
-                int num2 = Integer.parseInt(input2.getText().toString());
-                Toast.makeText(getApplicationContext(), num1 * num2, Toast.LENGTH_LONG).show();
+                int num1 = Integer.parseInt(String.valueOf(input.getText()));
+                int num2 = Integer.parseInt(String.valueOf(input2.getText()));
+                String result = (num1 * num2) + "";
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             }
         });
 
         btnDiv.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                int num1 = Integer.parseInt(input.getText().toString());
-                int num2 = Integer.parseInt(input2.getText().toString());
-                Toast.makeText(getApplicationContext(), num1 / num2, Toast.LENGTH_LONG).show();
+                int num1 = Integer.parseInt(String.valueOf(input.getText()));
+                int num2 = Integer.parseInt(String.valueOf(input2.getText()));
+                String result = (num1 / num2) + "";
+                Toast.makeText(getApplicationContext(), result, Toast.LENGTH_LONG).show();
             }
         });
 
+    }
 
+    public boolean isNumber(String str){
 
+        try {
+            Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            return false;
+        }
 
+        return true;
     }
 }
