bb24779038258056a507602b64fe915503b2d7af
USER <aa@aa.com>
2016-09-22 15:16:40 +0900

    modify3

---
+++ app/src/main/java/com/day0919/ActTabTest.java
@@ -0,0 +1,32 @@
+package com.day0919;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+
+public class ActTabTest extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.tabtest);
+        TabHost tab_host = (TabHost) findViewById(R.id.tabhost);
+        tab_host.setup();
+
+        TabHost.TabSpec ts1 = tab_host.newTabSpec("tab1");
+        ts1.setIndicator("tab1");
+        ts1.setContent(R.id.tab1);
+        tab_host.addTab(ts1);
+
+        TabHost.TabSpec ts2 = tab_host.newTabSpec("tab2");
+        ts2.setIndicator("tab2");
+        ts2.setContent(R.id.tab2);
+        tab_host.addTab(ts2);
+
+        TabHost.TabSpec ts3 = tab_host.newTabSpec("tab3");
+        ts3.setIndicator("tab3");
+        ts3.setContent(R.id.tab3);
+        tab_host.addTab(ts3);
+
+        tab_host.setCurrentTab(0);
+    }
+}
